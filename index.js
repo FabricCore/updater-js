c580124fe7880b49a8d31882d217b236e08548f7
siriusmart <siriusmart@tuta.io>
2025-07-18 15:37:11 +0100

    api error message

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ function checkUpdateSync(hasUpdate, noUpdate) {
         "https://api.github.com/repos/fabriccore/bootstrap.js/commits",
     );
     let json = res.json();
+
+    if (json.message) {
+        console.error(
+            json.message.replace(
+                /[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*/,
+                "[redacted IP address]",
+            ),
+        );
+        return;
+    }
+
     let latestHash = json[0].sha;
 
     let currentHash;
